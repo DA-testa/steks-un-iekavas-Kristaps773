f8bbc30f7dd611be7e6ee4be67122fd306e76b75
Kristaps773 <84283476+Kristaps773@users.noreply.github.com>
2023-02-17 22:31:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             if not opening_brackets_stack:
                 return i+1
-            if not are_matching(opening_brackets_stack.pop().char, next):
+            closing = opening_brackets_stack.pop()
+            if not are_matching(closing.char, next):
                 return i+1
             
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
         
-        else:
-            return "Success"
+    else:
+        return "Success"
 
 
 def main():
@@ -36,5 +37,5 @@ def main():
     # Printing answer, write your code here
     print(mismatch)
 
-if __name__ == "__main__":
+if __name__ in "__main__":
     main()
